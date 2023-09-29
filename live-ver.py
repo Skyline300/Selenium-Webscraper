b6fab6b2e69050a10589c830bd4d67997a635bbd
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2023-09-29 11:45:48 +0800

    Update live-ver.py

--- live-ver.py
+++ live-ver.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
+from dotenv import load_dotenv
 options = Options();
 options.add_experimental_option('excludeSwitches', ['enable-logging']);
 commands = {};
@@ -17,18 +17,24 @@ url = "https://track.mydronefleets.com/playback/64fd5648e98213f5d3f5b415";
 driver  = webdriver.Chrome(options=options);
 driver.get(url);
 
+load_dotenv()
 
+email = os.environ['email']
+password = os.environ['password']
+
+print(email)
+print(password)
 try:
     WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//div/button')))
 finally:
     res = driver.find_element(By.XPATH,'//div/h4')
-    print("the string:")
     print(res.text)
    # driver.quit()
 
 
 def _cock(*args):
     print("balls")
+    return
 
 
 def _login(*args):
@@ -36,16 +42,30 @@ def _login(*args):
     try: 
         WebDriverWait(driver,20).until(EC.presence_of_element_located((By.ID,'email_login')));
     finally: 
-        print("there is a login input")
+        driver.find_element(By.ID,'email_login').send_keys(email)
+        driver.find_element(By.ID,'nextButton').click()
+        WebDriverWait(driver,20).until(EC.presence_of_element_located((By.ID,'password')));
+        driver.find_element(By.ID,'password').send_keys(password)
+        driver.find_element(By.ID,'nextButton').click()
+
+
+def _get(*args):
+    try:
+        data = driver.find_element(By.CLASS_NAME,'jss85')
+        print(data.text)
+    except:
+        print("class not found")
+
 
 
 def _exit(*args):
     driver.quit()
+    return
 
 commands["exit"] = _exit
 commands["cock"] = _cock
 commands["login"] = _login
-
+commands["get"] = _get
 
 def repl():
     while True:
